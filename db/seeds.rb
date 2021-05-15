86aa2f7112737eefddd0ce0ccc95e16be430f9d6
Valentin Hitorin <valhitorin@gmail.com>
2021-05-15 17:52:05 +0200

    5 seeds added 5 users 5 posts 1 reaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,13 @@ Comment.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Testing category"
-user = User.create(email: "me@me.com", password: "123456", nickname: "blueberry", emoji: "😊", background_color: "blue")
-user_2 = User.create(email: "you@me.com", password: "123456", nickname: "blackberry", emoji: "😬", background_color: "black")
+puts "Testing"
+user_6 = User.create(email: "blu@me.com", password: "123456", nickname: "blueberry", emoji: "😅", background_color: "blue")
+user_7 = User.create(email: "bla@me.com", password: "123456", nickname: "blackberry", emoji: "🤠", background_color: "black")
+user_8 = User.create(email: "rasp@me.com", password: "123456", nickname: "raspberry", emoji: "🤪", background_color: "white")
+user_9 = User.create(email: "brick@me.com", password: "123456", nickname: "brickleberry", emoji: "🐶", background_color: "green")
+user_10 = User.create(email: "straw@me.com", password: "123456", nickname: "strawberry", emoji: "🥸", background_color: "red")
+
 
 first_category = Category.create(name: "Work and Study")
 second_category = Category.create(name: "Finance")
@@ -28,6 +32,10 @@ ninth_category = Category.create(name:  "Other Screw-ups")
 
 
 
-post = Post.create(user: user, content: "I screw up pretty big at home!", category: third_category)
-post.reactions.create(user: user_2)
-puts "Finished category"
+post = Post.create(user: user_6, content: "I screw up pretty big at Work!", category: first_category)
+post = Post.create(user: user_7, content: "I screw up pretty big at Finance!", category: second_category)
+post = Post.create(user: user_8, content: "I screw up pretty big at Household!", category: third_category)
+post = Post.create(user: user_9, content: "I screw up pretty big at Relationship!", category: fourth_category)
+post = Post.create(user: user_10, content: "I screw up pretty big at Family!", category: fifth_category)
+post.reactions.create(user: user_8)
+puts "Finished testing"
