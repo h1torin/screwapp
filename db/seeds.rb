bc88c57928406a094f5c109e6995374eaef39241
laura zerbini <lauraczerbini@gmail.com>
2021-05-15 17:09:30 +0200

    preview done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Testing category"
-user = User.create(email: "me@me.com", password: "123456", nickname: "blueberry", emoji: ":)", background_color: "blue")
+# user = User.create(email: "me@me.com", password: "123456", nickname: "blueberry", emoji: ":)", background_color: "blue")
 
-first_category = Category.create(name: "Work and Study")
-second_category = Category.create(name: "Finance")
-third_category = Category.create(name: "Household")
-fourth_category = Category.create(name: "Relationship")
-fifth_category = Category.create(name:  "Family")
-sixth_category = Category.create(name: "Friends")
-seventh_category = Category.create(name: "Health")
-eighth_category = Category.create(name: "Hobby and Leisure")
-ninth_category = Category.create(name:  "Other Screw-ups")
+# first_category = Category.create(name: "Work and Study")
+# second_category = Category.create(name: "Finance")
+# third_category = Category.create(name: "Household")
+# fourth_category = Category.create(name: "Relationship")
+# fifth_category = Category.create(name:  "Family")
+# sixth_category = Category.create(name: "Friends")
+# seventh_category = Category.create(name: "Health")
+# eighth_category = Category.create(name: "Hobby and Leisure")
+# ninth_category = Category.create(name:  "Other Screw-ups")
 
 
 
-post = Post.create(user_id: 5, content: "I screw up pretty big at home!", category_id: 11)
+post = Post.create(user_id: 1, content: "I screw up pretty big at home!", category_id: 5)
 puts "Finished category"
