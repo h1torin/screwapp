deb91d8a61ba80dec7fea417a0f6b442d1929a11
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-18 22:27:44 +0300

    Added model Tag, PostsTags to the application. The user can now add hashtags and view posts related to those hashtags.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,23 @@ class Post < ApplicationRecord
   has_many :reactions, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :photo
+  has_and_belongs_to_many :tags
+
+  after_create do
+    hashtags = self.content.scan(/#\w+/)
+    hashtags.uniq.map do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      self.tags << tag
+    end
+  end
+
+  before_update do
+    self.tags.clear
+    hashtags = self.content.scan(/#w*/)
+    hashtags.uniq.map do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      self.tags << tag
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'profile', to: 'users#profile', as: 'profile'
+  get '/posts/hashtag/:name', to: 'posts#hashtags', as: 'hashtag'
 
     resources :posts do
     resources :comments, only: [ :new, :create ] # doesn't really require new but we can keep it here for the time being.
