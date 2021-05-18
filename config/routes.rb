3609df881236842a85b79b9cc156febaa15dac8e
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-19 01:26:59 +0300

    Added search in the application. The user can now search for hashtags or words and will be routed to the search_index_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'search/index'
   devise_for :users
   root to: 'pages#home'
 
   get 'profile', to: 'users#profile', as: 'profile'
   get '/posts/hashtag/:name', to: 'posts#hashtags', as: 'hashtag'
+  get 'search/index', to: 'search#index'
 
     resources :posts do
     resources :comments, only: [ :new, :create ] # doesn't really require new but we can keep it here for the time being.
