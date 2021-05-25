28999376d69c305c31f6eed6b1a66bd2856a7cd6
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-25 19:37:18 +0200

    Fixed the search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,13 @@ class SearchController < ApplicationController
       @posts = Post.joins(:tags).where(tags: { name: query })
     else
       @posts = Post.where("content like ?", "%#{params[:query]}%")
+      @hashtags = Tag.where(name: params[:query])
     end
   end
+
+  # def hashtags
+  #   tag = Tag.find_by(name: params[:name])
+  #   @posts = tag.posts
+  #   authorize @posts
+  # end
 end
