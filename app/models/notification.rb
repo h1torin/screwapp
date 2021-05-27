2454528c1713467406c28e6b1299bcfe47fd3a2f
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-27 14:43:45 +0200

    commented out the noticed

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,4 @@
-class Notification < ApplicationRecord
-  include Noticed::Model
-  belongs_to :recipient, polymorphic: true
-end
+# class Notification < ApplicationRecord
+#   include Noticed::Model
+#   belongs_to :recipient, polymorphic: true
+# end
