f25ab7f94f7841cea3955cb7801fbfd6630e576c
laura zerbini <lauraczerbini@gmail.com>
2021-05-27 19:21:03 +0200

    last changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,9 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(resource)
+    posts_path # your path
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
