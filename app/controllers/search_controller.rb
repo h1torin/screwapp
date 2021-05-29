d1b2bfaa95e115d7b6a15644cc329960842acab6
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-29 17:23:21 +0200

    Working on the user post categories view for the profile page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,13 @@ class SearchController < ApplicationController
     @posts = policy_scope(Post)
     if params[:query].start_with?('#')
       @hashtags = Tag.where(name: params[:query])
+      # @category = Category.where(name: params[:query])
       query  = params[:query].downcase.gsub('#', '')
       @posts = Post.joins(:tags).where(tags: { name: query })
     else
       @posts = Post.where("content like ?", "%#{params[:query]}%")
       @hashtags = Tag.where(name: params[:query])
+      # @category = Category.where(name: params[:query])
     end
   end
 
