fcaca1173a5664b5fd920eb882c502184c14d726
laura zerbini <lauraczerbini@gmail.com>
2021-06-03 20:17:41 +0200

    notifications part 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,12 +4,13 @@ class Comment < ApplicationRecord
   # belongs_to :parent, class_name: 'Comment', optional: true
   # has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
   validates :content, presence: true
-
-  after_create_commit { notify }
+  # if self.user != post.user
+    after_create_commit { notify }
+  # end
 
   private
 
   def notify
-    Notification.create(event: "New Comment")
+    Notification.create(event: "New Comment") if user != post.user
   end
 end
