38e189bb36548abbff827a3f77f85970fb7edd31
laura zerbini <lauraczerbini@gmail.com>
2021-06-08 19:46:43 +0200

    notifications doneeee

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,22 +4,22 @@ class ReactionsController < ApplicationController
     reaction = Reaction.create(post_id: params["post_id"], user_id: current_user.id)
     if reaction.save
       if reaction.user != reaction.post.user
-        notification = Notification.create(event: "New Like")
+        notification = Notification.create(event: "New Like by #{reaction.user.nickname}")
         notification.update(user: reaction.post.user, reaction: reaction)
-        @counter = Notification.where(user: @reaction.post.user).where(status: false).count
+        @counter = Notification.where(user: reaction.post.user).where(status: false).count
         ActionCable.server.broadcast("general", "#{@counter}")
       end
-      redirect_to post_path(@post)
+      redirect_to posts_path
     else
-      render 'new'
+      if params["origin"] == "index"
+        redirect_to posts_path(anchor: "post-#{post.id}")
+      else
+        redirect_to post_path(post, anchor: "post-#{post.id}")
+      end
+      # redirect_to posts_path
     end
     authorize(reaction)
 
-    if params["origin"] == "index"
-      redirect_to posts_path(anchor: "post-#{post.id}")
-    else
-      redirect_to post_path(post, anchor: "post-#{post.id}")
-    end
   end
 
   def destroy
