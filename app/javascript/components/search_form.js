dfce37e5e0393c48f867a42ef96ef7f8c8bc6ebb
Rahman Shafiee <r.shafiee@gmx.de>
2021-06-08 20:41:39 +0200

    Fixed the javascript

--- app/javascript/components/search_form.js
+++ app/javascript/components/search_form.js
@@ -5,19 +5,28 @@ const searchForm = () => {
     const trendingHashtags = document.querySelector(".trending-hashtags");
     const notificationIcon = document.querySelector(".notification-counter");
     const inputField = document.querySelector(".input-field");
+    const clickables = document.querySelectorAll(".clickable")
+    const restOfArticle = document.querySelector("article.flex-grow-1")
     trendingHashtags.classList.add("d-block")
     notificationIcon.classList.add("d-none")
     inputField.classList.add("search-form-expanded")
+    restOfArticle.addEventListener("blur", () => {
+      trendingHashtags.classList.remove("d-block")
+      notificationIcon.classList.remove("d-none")
+      inputField.classList.remove("search-form-expanded")
+    })
 
   })
-  searchInput.addEventListener("blur", () => {
-    const trendingHashtags = document.querySelector(".trending-hashtags");
-    const notificationIcon = document.querySelector(".notification-counter");
-    const inputField = document.querySelector(".input-field");
-    trendingHashtags.classList.remove("d-block")
-    notificationIcon.classList.remove("d-none")
-    inputField.classList.remove("search-form-expanded")
-  })
+  // searchInput.addEventListener("blur", (e) => {
+  //   const trendingHashtags = document.querySelector(".trending-hashtags");
+  //   const notificationIcon = document.querySelector(".notification-counter");
+  //   const inputField = document.querySelector(".input-field");
+  //   const hashtagclickable = document.querySelector(".blur-hashtag")
+  //   console.log(e.target)
+
+  //   if (e.target != hashtagclickable) {
+  //   }
+  // })
 };
 
 export { searchForm };
